bb02749e0945696e44a830e064b85755174f43b2
R.Kulemin <R.Kulemin@PC-KULEMIN-RA>
2026-04-28 12:10:44 +0400

    Binding

---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Binding
+{
+    /*
+    Binding - позволяет привязывать переменные к графическим интерфейсам таким образом 
+    чтобы элементы интерфейса отображали значения этих переменных, а иногда и изменяли эти значения.
+
+    К элементами интерфейса привязываются не сами переменные, а свойства (Propeties) соответствующие 
+    к этим переменным
+     */
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        string boundText;
+        public string BoundText 
+        {
+            get => boundText;
+            set
+            {
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            txtInput.Focus();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+        }
+
+        private void BtnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Default";
+        }
+    }
+}
